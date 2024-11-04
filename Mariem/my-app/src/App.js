3f763a8a98c77e9ac3533240a9dc291d173bbe3f
GarismoHussien <hand2land@gmail.com>
2024-11-04 19:28:36 +0200

    T

--- Mariem/my-app/src/App.js
+++ Mariem/my-app/src/App.js
@@ -3,6 +3,11 @@ import React from 'react';
 function MyComponent() {
     const greeting = "Hello, World!";
     
+    // Move handleClick function above the return statement
+    function handleClick() {
+        alert("Button clicked!");
+    }
+
     return (
         <div>
             <h1>{greeting}</h1>
@@ -10,10 +15,23 @@ function MyComponent() {
             <button onClick={handleClick}>Click Me</button>
         </div>
     );
-    
-    function handleClick() {
-        alert("Button clicked!");
-    }
 }
 
+function Greeting() {
+    const name = "Ahmed";
+    const isLoggedIn = true;
+
+    return (
+        <div>
+            <h1>Hello, {name}</h1>
+            <p>Welcome back!</p>
+
+            {isLoggedIn ? <button>Logout<button> : <button>Login</button>
+            
+            <img src="welcome.png">
+        </div>
+    );
+}
+
+export default Greeting;
 export default MyComponent;
