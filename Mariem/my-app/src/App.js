5a4f5f58d51251f2af0e3693bf062ecd8e978d09
“mariam-alhindassi” <Maryam_s.m@hotmail.com>
2024-11-04 21:38:12 +0400

    update

--- Mariem/my-app/src/App.js
+++ Mariem/my-app/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 
-<<<<<<< HEAD
 function App() {
   return (
     <div className="App">
@@ -24,40 +23,3 @@ function App() {
 }
 
 export default App;
-=======
-function MyComponent() {
-    const greeting = "Hello, World!";
-    
-    // Move handleClick function above the return statement
-    function handleClick() {
-        alert("Button clicked!");
-    }
-
-    return (
-        <div>
-            <h1>{greeting}</h1>
-            <p>Welcome to our site.</p>
-            <button onClick={handleClick}>Click Me</button>
-        </div>
-    );
-}
-
-function Greeting() {
-    const name = "Ahmed";
-    const isLoggedIn = true;
-
-    return (
-        <div>
-            <h1>Hello, {name}</h1>
-            <p>Welcome back!</p>
-
-            {isLoggedIn ? <button>Logout<button> : <button>Login</button>
-            
-            <img src="welcome.png">
-        </div>
-    );
-}
-
-export default Greeting;
-export default MyComponent;
->>>>>>> refs/remotes/origin/main
