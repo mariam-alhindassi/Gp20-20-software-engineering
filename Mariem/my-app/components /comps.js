0eafc5f58443758b8aad07afbf8a40f6e6452880
“mariam-alhindassi” <Maryam_s.m@hotmail.com>
2024-11-13 17:26:38 +0400

    fun

---
+++ Mariem/my-app/components /comps.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const comps = () => <h1> Hi this is Mariam </h1>
+
+export default comps
--- Mariem/my-app/src/App.js
+++ Mariem/my-app/src/App.js
@@ -1,46 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import mycomp from './components/comps';  // Ensure this path is correct
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hellow This is Mariam 
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
       
-      {/* Include Greeting component here */}
-      <Greeting />
-    </div>
-  );
-}
+//       {/* Include Greeting component here */}
+//       <Greeting />
+//     </div>
+//   );
+// }
 
-function Greeting() {
-  const name = "Ahmed";
-  const isLoggedIn = true;
+// function Greeting() {
+//   const name = "Ahmed";
+//   const isLoggedIn = true;
 
-  return (
-    <div>
-      <h1>Hello, {name}</h1>
-      <p>Welcome back!</p>
+//   return (
+//     <div>
+//       <h1>Hello, {name}</h1>
+//       <p>Welcome back!</p>
 
-      {isLoggedIn ? <button>Logout</button> : <button>Login</button>}
+//       {isLoggedIn ? <button>Logout</button> : <button>Login</button>}
       
-      {/* Self-close the img tag */}
-      <img src="welcome.png" alt="welcome" />
-    </div>
-  );
+//       {/* Self-close the img tag */}
+//       <img src="welcome.png" alt="welcome" />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+class App extends Component {
+  render() {
+    return(
+      <div className='App'>
+        <mycomp> 
+
+        </mycomp>
+      </div>
+    );
+    
+  }
 }
 
 export default App;
